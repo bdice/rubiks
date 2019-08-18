31c7c95371d5bac1daa543ee9260e61b0872aebc
Bradley Dice <bdice@bradleydice.com>
2019-08-17 23:39:34 -0400

    Scramble and unscramble the cube.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <random>
 #include "Rubiks.h"
 
 using namespace std;
@@ -6,7 +7,23 @@ using namespace std;
 int main()
 {
     auto cube = Rubiks();
-    cube.move(0);
+    default_random_engine generator;
+    uniform_int_distribution<int> distribution(0, 11);
+    vector<int> move_history;
+    for (int i=0; i < 1000; i++)
+    {
+        int move = distribution(generator);
+        move_history.push_back(move);
+        cube.move(move);
+    }
+
+    cube.printCube();
+
+    for (auto it = move_history.rbegin(); it != move_history.rend(); it++)
+    {
+        cube.move(Rubiks::reverseMove(*it));
+    }
+
     cube.printCube();
     cout << "Done." << endl;
 }
